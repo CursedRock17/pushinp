5c51e56498d3f7982c0048f9aace993e148f3f03
CursedRock17 <mtglucas1@gmail.com>
2022-10-07 21:52:51 -0400

    Test Pusher App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Pusher from 'pusher-js'
 
 function App() {
+  const bob = () => {
+      
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+  
+    var pusher = new Pusher('93bc0f8bfdfe83f0c6a9', {
+      cluster: 'us2'
+    });
+
+    var channel = pusher.subscribe('my-channel');
+    channel.bind("pusher:subscription_count", (data) => {
+      console.log(data.subscription_count);
+      console.log(channel.subscription_count);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
+      <h1>Pusher Test</h1>
+      <button
+      onClick={(e) => bob(e.target.value)}
+      >
+        Bob
+      </button>
+    </>
   );
 }
 
